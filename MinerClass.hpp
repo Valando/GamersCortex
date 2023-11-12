34d32f5b5f633e694030a10e1bad901a2d2210d3
Valando <brdza15@gmail.com>
2023-11-12 17:56:13 +0100

    hpp

---
+++ MinerClass.hpp
@@ -0,0 +1,17 @@
+#ifndef MINERCLASS_HPP
+#define MINERCLASS_HPP
+
+class MinerClass {
+public:
+    // Constructor
+    MinerClass(int initialValue);
+
+    // Member function
+    void printValue() const;
+
+private:
+    // Private data member
+    int value;
+};
+
+#endif // MINERCLASS_HPP
