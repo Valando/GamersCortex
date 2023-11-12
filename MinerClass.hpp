5d466b92d14d47b7d53f9fab2d77456d8114ed99
Valando <brdza15@gmail.com>
2023-11-12 19:21:36 +0100

    Rectangle hpp

--- MinerClass.hpp
+++ MinerClass.hpp
@@ -1,17 +1,25 @@
 #ifndef MINERCLASS_HPP
 #define MINERCLASS_HPP
+#include <SDL2/SDL.h>
 
 class MinerClass {
 public:
     // Constructor
     MinerClass(int initialValue);
+    SDL_Rect* rect;
 
     // Member function
     void printValue() const;
 
+       // makes a rectangle r on the x,y plane, with widh w, and height h
+    void GenerateRectangle(int xr, int yr, int wr, int hr);
+    
 private:
     // Private data member
     int value;
+    SDL_Window* vindue ;
+    SDL_Renderer* renderer;
+    
 };
 
 #endif // MINERCLASS_HPP
