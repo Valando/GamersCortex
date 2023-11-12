84bfbd880ff9a67fc2590bfa446bb8b24d51478b
Valando <brdza15@gmail.com>
2023-11-12 15:59:38 +0100

    w

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <SDL2/SDL.h>
+#include <string>
+#include <iostream>
+using namespace std;
+int main() {
+    // Your code goes here
+    
+    string a = "hello";
+
+    cout << a << endl;
+    return 0;
+}
